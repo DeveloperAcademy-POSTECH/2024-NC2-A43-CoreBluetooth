c68a5e58c9156fb27a4de92b1eda07c696937f8e
pil_gaaang <21900204@handong.ac.kr>
2024-06-19 17:20:57 +0900

    ui, bt connected

--- BongSoo/BongSoo/SecondPage.swift
+++ BongSoo/BongSoo/SecondPage.swift
@@ -12,18 +12,36 @@ struct SecondPage: View {
     }
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text("Device Name: \(deviceName)")
-                .font(.largeTitle)
-                .bold()
-                .padding(.top, 16)
-                .padding(.horizontal)
-
-            Text("RSSI: \(rssi)")
-                .font(.system(size: 24, weight: .semibold))
-                .padding(.horizontal)
-
-            Spacer()
+        ZStack {
+            RadialGradient(gradient: Gradient(colors: [Color.gray.opacity(0.6), Color.yellow.opacity(0.8)]), center: .center, startRadius: 140, endRadius: 170)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                
+                VStack {
+                    Text("주의")
+                        .font(.system(size: 80))
+                        .fontWeight(.bold)
+                    
+                    Text("피보호자가 근처에 있습니다.")
+                        .font(.system(size: 25))
+                }
+                .foregroundColor(.yellow)
+                
+                Spacer()
+                
+                VStack {
+                    Text("\(deviceName)과 연결되어 있습니다.")
+                        .font(.system(size: 20))
+                    
+                    Text("신호강도 : \(rssi + 100)")
+                        .font(.system(size: 25))
+                        .fontWeight(.bold)
+                }
+                .padding(.bottom)
+            }
+            .padding(20)
         }
         .background(Color("AttentionColor"))
         .navigationBarTitleDisplayMode(.inline)
@@ -41,31 +59,23 @@ struct SecondPage: View {
 
     private func updateNavigation(rssi: Int?) {
         if let rssi = rssi {
-            if rssi < -85 || rssi >= -65 {
-                navigateToAppropriatePage(rssi: rssi)
-            }
-        } else {
-            navigateToAppropriatePage(rssi: rssi)
-        }
-    }
-
-    private func navigateToAppropriatePage(rssi: Int?) {
-        if let rssi = rssi {
-            if rssi >= -65 {
+            if rssi >= -55 {
                 UIApplication.shared.windows.first?.rootViewController = UIHostingController(rootView: FirstPage(bluetoothManager: bluetoothManager, deviceName: deviceName, rssi: rssi))
-            } else if rssi >= -85 {
-                // Remain on SecondPage
-            } else {
+            } else if rssi < -65 {
                 UIApplication.shared.windows.first?.rootViewController = UIHostingController(rootView: ThirdPage(bluetoothManager: bluetoothManager, deviceName: deviceName, rssi: rssi))
             }
         } else {
-            UIApplication.shared.windows.first?.rootViewController = UIHostingController(rootView: FourthPage(bluetoothManager: bluetoothManager, deviceName: deviceName, rssi: nil))
+            navigateToDisconnectionPage()
         }
+    }
+
+    private func navigateToDisconnectionPage() {
+        UIApplication.shared.windows.first?.rootViewController = UIHostingController(rootView: FourthPage(bluetoothManager: bluetoothManager, deviceName: deviceName, rssi: nil))
     }
 }
 
 struct SecondPage_Previews: PreviewProvider {
     static var previews: some View {
-        SecondPage(bluetoothManager: BluetoothManager(), deviceName: "Sample Device", rssi: -60)
+        SecondPage(bluetoothManager: BluetoothManager(), deviceName: "Sample Device", rssi: -75)
     }
 }
